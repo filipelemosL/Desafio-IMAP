2b78bf6b284255bb7a5ea4c53f0fb5969f08f515
Filipe Lemos <filipelemos1415@gmail.com>
2024-01-24 14:46:37 -0300

    commit inicial

---
+++ IMAP-FrontEnd/src/Components/Menu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Menu = () => {
+ return(
+    <header>
+        <div className="background">
+            <div className="mainContainer">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </header>
+ )
+}
